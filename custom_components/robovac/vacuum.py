00f0be3cfbb272019327b43f35806c4f57ac987e
Alex S. <Ascendor@users.noreply.github.com>
2026-01-09 00:26:30 +0100

    syntax

--- custom_components/robovac/vacuum.py
+++ custom_components/robovac/vacuum.py
@@ -395,7 +395,6 @@ class RoboVacEntity(StateVacuumEntity):
         self.tuya_state = None
         self.tuyastatus = None
         await self.async_forced_update()
-()
 
     async def async_added_to_hass(self):
         await self.async_forced_update()
